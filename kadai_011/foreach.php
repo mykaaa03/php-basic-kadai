2376277012cae735f4e4eae4700c2fa5d12d9e22
mykaaa03 <mykaaa0327@gmail.com>
2024-09-03 23:11:50 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP課題11</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $product_data = ['名前' => '玉ねぎ', '価格' => 200, '産地' => '北海道'];
+ 
+         // 連想配列$personal_dataのキーと値を1つずつ順番に出力する
+         foreach ($product_data as $key => $value) {
+            echo $key . ':' . $value . "<br>";
+         }
+         ?>
+
+
+     </p>
+ </body>
+ 
+ </html>
